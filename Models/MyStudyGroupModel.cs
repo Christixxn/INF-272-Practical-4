8c6b35d27608e58813cab5507b582e1ae179afaa
Christixxn <u24614794@tuks.co.za>
2025-04-07 18:46:55 +0200

    Add files via upload

---
+++ Models/MyStudyGroupModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace myStudyGroup.Models
+{
+	public class MyStudyGroupModel
+	{
+
+		[Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; } 
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Personal Page")]
+        public string MyLink { get; set; }
+
+
+    }
+
+   
+}
